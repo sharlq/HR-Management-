b55856388d389a0008557d6e20e4954ed8f5dd54
sharlq <shehabakhader@gmail.com>
2021-06-08 17:45:35 +0300

    created the leave request

--- components/services/LeaveRequest.tsx
+++ components/services/LeaveRequest.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 const LeaveRequest = () => {
   return (
     <div className="leaveRequest">
@@ -13,10 +13,16 @@ const LeaveRequest = () => {
         <TextField label="Leave Type" variant="outlined" />
         <div>
           <h4>Have you informed your manager?</h4>
-          <ButtonGroup color="default" aria-label="outlined primary button group">
-        <Button color="primary" variant="contained">YES</Button>
-        <Button>NO</Button>
-      </ButtonGroup>
+          <ButtonGroup
+            className='btnGroup'
+            color="default"
+            aria-label="outlined primary button group"
+          >
+            <Button color="primary" variant="contained">
+              YES
+            </Button>
+            <Button>NO</Button>
+          </ButtonGroup>
         </div>
         <TextField label="Reason Of leave" variant="outlined" />
         <Button className="btn" variant="contained" color="primary">
