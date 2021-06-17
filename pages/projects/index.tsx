8550ee7e648722e7e385883238a0edf4d098209a
sharlq <shehabakhader@gmail.com>
2021-06-17 19:07:50 +0300

    created the a stamp color for eaxh object

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,18 +2,25 @@ import React,{useState,useEffect} from "react";
 import Projects from "../../components/projects/Projects";
 import SideBar from '../../components/projects/SideBar'
 import PopUpAddProject from "../../components/projects/PopUpAddProject";
+import axios from "axios";
 const Index = () => {
   const [trigger,setTrigger] = useState(false)
+  const [projects,setProjects] = useState<any>()
   const handleAddProject =()=>{
         setTrigger((prev)=>!prev)
   }
-  const handleFetchProjects = ()=> {
-    
+  const handleFetchProjects = async()=> {
+    let res = await  axios.get('http://localhost:3000/api/projects')
+   await setProjects(res.data)
   }
+  useEffect(()=>{
+    handleFetchProjects();
+    console.log(projects)
+  },[])
   return (
     <div className="projectsPage">
       <div className="mainContent">
-        <SideBar handleTrigger={handleAddProject}  />
+        <SideBar handleTrigger={handleAddProject} projects={projects}  />
         <Projects />
       </div>
         <PopUpAddProject handleTrigger={handleAddProject} trigger={trigger} />
