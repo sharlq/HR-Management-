4f47e0ba00118a049d0ef0c85d4c29c56f5cc636
sharlq <shehabakhader@gmail.com>
2021-06-22 21:48:46 +0300

    create the functionality of deleteing project

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -3,17 +3,25 @@ import Tasks from "./tasks";
 import { useSelector } from "react-redux";
 import { selectSelectedProject } from "../../redux/features/projectsSlice";
 import ClearIcon from '@material-ui/icons/Clear';
+import axios from "axios";
 
 const Projects:React.FC= () => {
 const project = useSelector(selectSelectedProject)
-
+const handleDeleteProject= ()=>{
+ axios.delete(`http://localhost:3000/api/projects?projectId=${project._id}`)
+  window.location.reload()
+}
 
   return (
     <div className="projects">
-      <div className="projects-title" >
+     {project? 
+     <div className="projects-title" >
       <h3>{project.projectName}</h3>
-
+      <ClearIcon style={{cursor:"pointer"}} onClick={()=>handleDeleteProject()} />
       </div>
+      :
+      <h3>Project Name</h3>
+      }
       <div className="projects-workflow">
         <Tasks title={"To Do"}/>
         <Tasks title={"Doing"}/>
