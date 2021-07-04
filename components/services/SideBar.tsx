87c0e64e168a040dcc66cef4cdf3c5cba5c8fccb
sharlq <shehabakhader@gmail.com>
2021-07-04 16:36:37 +0300

    change the stamp when changning the service pag

--- components/services/SideBar.tsx
+++ components/services/SideBar.tsx
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react'
 import Link from 'next/Link'
 import {useSelector} from 'react-redux'
 import { selectRank } from '../../redux/features/userSlice'
+
 const SideBar = () => {
     const [stampClass,setStampClass] = useState({
         workingSchdual:"stamp active",
@@ -11,25 +12,44 @@ const SideBar = () => {
     const [isManager,setIsManager] = useState(false)
     const [managerSectionClass,setManagerSectionClass] = useState('dontDisplay')
     const rank = useSelector(selectRank)
+    
+
+
     const verifyManagerStatus = () =>{
         if(rank=='topManagement')setIsManager(true)
         if(isManager) setManagerSectionClass("services-sidebar_manager")
-        console.log(rank,isManager)
+    }
+
+    const setStampForSelectedPage = (servicePageName)=>{
+        const stampClassProperties = ['workingSchdual','leaveRequest','showLeaveRequests']
+        let tempToSetStampClasses ={
+        workingSchdual:"stamp",
+        leaveRequest:"stamp",
+        showLeaveRequests:"stamp"
+        }
+        for(let i = 0 ; i<stampClassProperties.length;i++){
+            if(servicePageName===stampClassProperties[i]){
+                tempToSetStampClasses[stampClassProperties[i]]='stamp active'
+            }else{   
+                tempToSetStampClasses[stampClassProperties[i]]='stamp'
+            }
+        }
+        setStampClass(tempToSetStampClasses)
     }
 
     useEffect(()=>{
         verifyManagerStatus();
-    },[rank])
+    })
     return (
         <div className="services-sidebar">
             <div className="services-sidebar_employee">
                 <h3>Employee services</h3>
                 <ul>
-                <div className="listItem">
+                <div className="listItem" onClick={()=>setStampForSelectedPage('workingSchdual')}>
                 <div className={stampClass.workingSchdual}/>
                     <li>Working Schdual</li>
                 </div>
-                <div className="listItem">
+                <div className="listItem" onClick={()=>setStampForSelectedPage('leaveRequest')}>
                     <div className={stampClass.leaveRequest}/>
                     <li>Leave Request</li>
                 </div>
@@ -38,7 +58,7 @@ const SideBar = () => {
             <div className={managerSectionClass}>
                 <h3>Manager section</h3>
                 <Link href='services/showLeaveRequests'>
-                <div className="listItem" >
+                <div className="listItem" onClick={()=>setStampForSelectedPage('showLeaveRequests')} >
                 <div className={stampClass.showLeaveRequests} />
                 <li>Show Leave Requests</li>
                 </div>
